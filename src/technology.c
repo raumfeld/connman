c9b73d03d0864cec9cb085316071ef72b6a803d0
Oliver Tappe <zooey@hirschkaefer.de>
2015-03-10 14:17:56 +0100

    Accept empty passphrases.

--- src/technology.c
+++ src/technology.c
@@ -988,9 +988,6 @@ static DBusMessage *set_property(DBusConnection *conn,
 		if (technology->type != CONNMAN_SERVICE_TYPE_WIFI)
 			return __connman_error_not_supported(msg);
 
-		if (strlen(str) < 8 || strlen(str) > 63)
-			return __connman_error_passphrase_required(msg);
-
 		if (g_strcmp0(technology->tethering_passphrase, str) != 0) {
 			g_free(technology->tethering_passphrase);
 			technology->tethering_passphrase = g_strdup(str);
