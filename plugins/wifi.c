b3bc1d07b5e87ea9f55ab2ba843c3d02383371cc
Oliver Tappe <zooey@hirschkaefer.de>
2015-04-20 16:27:54 +0200

    wifi: log interface used for wifi tethering.

--- plugins/wifi.c
+++ plugins/wifi.c
@@ -3056,6 +3056,7 @@ static int tech_set_tethering(struct connman_technology *technology,
 			continue;
 		}
 
+		DBG("using %s as wifi tethering device", ifname);
 		mode = g_supplicant_interface_get_mode(interface);
 		if ((mode & G_SUPPLICANT_CAPABILITY_MODE_AP) == 0) {
 			DBG("%s does not support AP mode", ifname);
