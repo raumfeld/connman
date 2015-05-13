eb100816a7cdbc7f2acfc426a0d54edc56a9541d
Oliver Tappe <zooey@hirschkaefer.de>
2015-05-13 10:55:49 +0200

    tethering: add wifi interface to bridge as soon as tethering starts.
    * This matches the code when disabling tethering, as that expects
      to be able to remove the wifi interface from the bridge again, which
      fails when no client has ever connected to the AP. In the latter case,
      failing to remove the interface from the bridge caused the AP to
      remain open (visible, but no more functional).
    * Additionally, only try to remove the wifi interface from the bridge
      when the member variable wifi->bridge indicates that is has been 
      added to it before, which should avoid spurious errors in the log.

--- plugins/wifi.c
+++ plugins/wifi.c
@@ -3030,9 +3030,11 @@ static int tech_set_tethering(struct connman_technology *technology,
 			if (wifi->tethering) {
 				wifi->tethering = false;
 
-				connman_inet_remove_from_bridge(wifi->index,
+				if (wifi->bridged) {
+					connman_inet_remove_from_bridge(wifi->index,
 									bridge);
-				wifi->bridged = false;
+					wifi->bridged = false;
+				}
 			}
 		}
 
@@ -3086,6 +3088,7 @@ static int tech_set_tethering(struct connman_technology *technology,
 		}
 
 		info->wifi->tethering = true;
+		handle_tethering (wifi);
 
 		err = g_supplicant_interface_remove(interface,
 						sta_remove_callback,
